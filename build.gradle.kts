46a34670c69e847d8a4cdb6783e63d93f83e092c
崔江枫 <18814321456@163.com>
2024-11-22 10:09:29 +0800

    feat(plugin): 添加 ReaderBook 插件基础功能
    - 更新 Gradle 配置，使用 org.jetbrains.intellij.platform 插件
    - 添加 Intellij IDEA Ultimate 本地路径依赖- 配置插件信息和描述
    - 新增 ReaderBookAction 类，实现插件基本功能- 更新 .gitignore 文件，忽略 IntelliJ IDEA 相关文件

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,48 @@
 plugins {
-    id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
 }
-
 group = "cn.cuijiangfeng.readerbook"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+//    gradlePluginPortal()
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public")
+    }
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2023.2.6")
-    type.set("IC") // Target IDE Platform
 
-    plugins.set(listOf(/* Plugin Dependencies */))
+
+dependencies {
+    intellijPlatform {
+        //intellijIdeaCommunity("2024.2.4")
+        //bundledPlugin("com.intellij.java")
+        local("C:\\Users\\cjf\\AppData\\Local\\Programs\\IntelliJ IDEA Ultimate 3")
+        zipSigner()
+        instrumentationTools()
+    }
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+        // 解决编译时中文报错
+        options.encoding = "UTF-8"
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+
+    // 添加以下内容，解决运行时控制台中文乱码
+    withType<JavaExec> {
+        jvmArgs = listOf("-Dfile.encoding=UTF-8", "-Dsun.stdout.encoding=UTF-8", "-Dsun.stderr.encoding=UTF-8")
     }
 
     patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("242.*")
+        sinceBuild.set("242.*")
     }
 
     signPlugin {
---
+++ src/main/java/cn/cuijiangfeng/readerbook/readerbook/ReaderBookAction.java
@@ -0,0 +1,13 @@
+package cn.cuijiangfeng.readerbook.readerbook;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ReaderBookAction extends AnAction {
+    
+    
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        System.out.println("ReaderBookAction");
+    }
+}
