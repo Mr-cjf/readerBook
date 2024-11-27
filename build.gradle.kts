5841e044753aa3c1030a46d9ab35f9ba8e8e78e8
崔江枫 <18814321456@163.com>
2024-11-27 09:07:09 +0800

    feat(readerbook): 添加本地阅读功能并优化插件结构
    - 新增 LocalReaderBookAction 类实现本地文件阅读功能
    - 更新 ReaderBookAction 类，改为打开二级菜单- 修改 .gitignore 文件，排除 IntelliJ 平台相关文件
    - 更新 build.gradle.kts，添加依赖并配置插件版本
    - 新增 logback.xml 配置日志输出
    - 更新 plugin.xml，添加新的菜单项和动作

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,12 +15,23 @@ repositories {
     }
 }
 
-
+val commonsLangVersion: String by extra("3.17.0")
+val jUniversalChardetVersion: String by extra("1.0.3")
+val slf4jVersion: String by extra("2.0.16")
+val logbackVersion: String by extra("1.5.12")
+val lombokVersion: String by extra("1.18.34")
 
 dependencies {
+    implementation("org.apache.commons:commons-lang3:${commonsLangVersion}")
+    implementation("com.googlecode.juniversalchardet:juniversalchardet:${jUniversalChardetVersion}")
+    implementation("org.slf4j:slf4j-api:${slf4jVersion}")
+    implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+    implementation("org.projectlombok:lombok:${lombokVersion}")
+    annotationProcessor("org.projectlombok:lombok:${lombokVersion}") // 添加 Lombok 注解处理器
     intellijPlatform {
-        //intellijIdeaCommunity("2024.2.4")
-        //bundledPlugin("com.intellij.java")
+        // intellijIdeaCommunity("2024.2.4")
+        // intellijIdeaUltimate("2024.2.4")
+        bundledPlugin("com.intellij.java")
         local("C:\\Users\\cjf\\AppData\\Local\\Programs\\IntelliJ IDEA Ultimate 3")
         zipSigner()
         instrumentationTools()
@@ -42,7 +53,8 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("242.*")
+        sinceBuild.set("242.23726.103") // 修改为当前 IDE 版本
+        untilBuild.set("243.*") // 可以设置一个范围，确保兼容未来的小版本更新
     }
 
     signPlugin {
--- src/main/java/cn/cuijiangfeng/readerbook/readerbook/ReaderBookAction.java
+++ src/main/java/cn/cuijiangfeng/readerbook/readerbook/ReaderBookAction.java
@@ -2,12 +2,17 @@ package cn.cuijiangfeng.readerbook.readerbook;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class ReaderBookAction extends AnAction {
     
     
     @Override
-    public void actionPerformed(AnActionEvent e) {
-        System.out.println("ReaderBookAction");
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        System.out.println("打开二级菜单");
+        
     }
+    
+    
 }
+
